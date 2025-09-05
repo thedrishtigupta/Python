24a7302b745ad7540bb4ef2d8158cc5257106cfa
Drishti Gupta <thedrishtigupta@gmail.com>
2025-09-05 13:41:35 +0530

    find function in str

--- strings.py
+++ strings.py
@@ -1,9 +1,9 @@
 # To input a user's name and print good afternoon
 
-name = input("Enter  your name: ")
-print("Good Afternoon,", name) #No need to add extra space after Afternoon,", it automatically does it
-print(f"Good Afternoon, {name}") # use f for template literal
-date = input("Enter a date: ")
+# name = input("Enter  your name: ")
+# print("Good Afternoon,", name) #No need to add extra space after Afternoon,", it automatically does it
+# print(f"Good Afternoon, {name}") # use f for template literal
+# date = input("Enter a date: ")
 
 # This doesn't work
 # letter = """
@@ -11,10 +11,15 @@ date = input("Enter a date: ")
 #     You are Selected!
 #     {date}"""
 
-letter = """
-    Dear {name},
-    You are Selected!
-    {date}"""
-print(letter.replace("|<name>|", name).replace("|<date>|", date)) #this work
+# letter = """
+#     Dear {name},
+#     You are Selected!
+#     {date}"""
+# print(letter.replace("|<name>|", name).replace("|<date>|", date)) #this work
 
-print(letter)
+# print(letter)
+
+
+doublestr = "This contains   double   spaces"
+print(doublestr.find("  ")) #finds the index of first double space, or -1 if not found
+
