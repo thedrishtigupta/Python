40948ee15a347f1695e0dfbb82e32c9f2fd5f013
Drishti Gupta <thedrishtigupta@gmail.com>
2026-06-15 21:36:49 +0530

    Added code to csv file

--- Web Scraping/coreyms-scrape.py
+++ Web Scraping/coreyms-scrape.py
@@ -1,30 +1,62 @@
 
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 source = requests.get('http://coreyms.com').text
 
 soup = BeautifulSoup(source, 'lxml')
 
-# print(soup.prettify())
+csv_file = open('cms_scrape.csv', 'w')
 
-article = soup.find('article')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['headline', 'summary', 'video_link'])
 
-# print(article.prettify())
+for article in soup.find_all('article'):
+    headline = article.h2.a.text
+    print(headline)
 
-headline = article.h2.a.text
-print(headline)
+    summary = article.find('div', class_='entry-content').p.text
+    print(summary)
 
-summary = article.find('div', class_='entry-content').p.text
-print(summary)
+    try:
+        vid_src = article.find('iframe', class_='youtube-player')['src']
 
-vid_src = article.find('iframe', class_='youtube-player')['src']
-# print(vid_src)
+        vid_id = vid_src.split('/')[4]
+        vid_id = vid_id.split('?')[0]
 
-vid_id = vid_src.split('/')[4]
-# print(vid_id)
-vid_id = vid_id.split('?')[0]
-# print(vid_id)
+        yt_link = f'https://youtube.com/watch?v={vid_id}'
+    except Exception as e:
+        yt_link = None
 
-yt_link = f'https://youtube.com/watch?v={vid_id}'
-print(yt_link)
+    print(yt_link)
+
+    print()
+
+    csv_writer.writerow([headline, summary, yt_link])
+
+csv_file.close()
+
+# # print(soup.prettify())
+
+# article = soup.find('article')
+
+# # print(article.prettify())
+
+# headline = article.h2.a.text
+# print(headline)
+
+# summary = article.find('div', class_='entry-content').p.text
+# print(summary)
+
+# vid_src = article.find('iframe', class_='youtube-player')['src']
+# # print(vid_src)
+
+# vid_id = vid_src.split('/')[4]
+# # print(vid_id)
+# vid_id = vid_id.split('?')[0]
+# # print(vid_id)
+
+# yt_link = f'https://youtube.com/watch?v={vid_id}'
+# print(yt_link)
+
