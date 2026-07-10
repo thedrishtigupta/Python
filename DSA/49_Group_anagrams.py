cb369db974752a1ea07a8dd76a1fa5d47662e889
Drishti Gupta <thedrishtigupta@gmail.com>
2026-07-10 13:46:58 +0530

    Hashing: group anagrams in py

---
+++ DSA/49_Group_anagrams.py
@@ -0,0 +1,11 @@
+
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp = defaultdict(list)
+
+        for s in strs:
+            key = tuple(sorted(s))
+            mp[key].append(s)
+        
+        return list(mp.values())
